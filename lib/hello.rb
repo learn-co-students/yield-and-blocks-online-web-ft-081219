bedec95b06c8c3ae13c5feb87784b123d287ca4b
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-06 17:00:51 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(array)
+    if block_given?
+        array.collect {|el| yield el}
+    else 
+        puts "Hey! No block was given!"
+    end
+    return array
 end
+
+#ternary also works, but has to be in parenthesis
+# def hello_t(array)
+#     block_given? ? (array.collect {|el| yield el}) : (puts "Hey! No block was given!")
+#     return array
+# end
 
 # call your method here!
 
